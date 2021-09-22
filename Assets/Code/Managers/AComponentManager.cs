52fd6e0a891ba13f041d58f30bf74799d304d30b
Vheos <Vheos777@gmail.com>
2021-09-22 23:49:43 +0200

    added generic component manager

---
+++ Assets/Code/Managers/AComponentManager.cs
@@ -0,0 +1,34 @@
+namespace Vheos.Games.ActionPoints
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Tools.UnityCore;
+    using System.Linq;
+    [DisallowMultipleComponent]
+    abstract public class AComponentManager<T> : AUpdatable where T : Behaviour
+    {
+        // Publics
+        static public T FirstActive
+        => _components.FirstOrDefault(c => c != null && c.isActiveAndEnabled);
+        static public T AddComponentTo(GameObject t)
+        {
+            T newComponent = t.AddComponent<T>();
+            _components.Add(newComponent);
+            return newComponent;
+        }
+        static public T AddComponentTo(Component t)
+        => AddComponentTo(t.gameObject);
+
+        // Privates
+        static protected AComponentManager<T> _instance;
+        static protected List<T> _components;
+
+        // Mono
+        public override void PlayAwake()
+        {
+            base.PlayAwake();
+            _instance = this;
+            _components = new List<T>(FindObjectsOfType<T>(true));
+        }
+    }
+}
