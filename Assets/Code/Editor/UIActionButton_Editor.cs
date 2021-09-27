41baba26f43b46185b6c53bc4b7bf01dd69fe7c3
Vheos <Vheos777@gmail.com>
2021-09-27 04:41:08 +0200

    added editor for 'UIActionButton'

---
+++ Assets/Code/Editor/UIActionButton_Editor.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_EDITOR
+namespace Vheos.Games.ActionPoints.Editor
+{
+    using UnityEditor;
+    using UnityEditor.UI;
+    [CustomEditor(typeof(UIActionButton), true)]
+    [CanEditMultipleObjects]
+    public class UIActionButton_Editor : ButtonEditor
+    {
+        SerializedProperty __HighlightedScale, __ScaleAnimation;
+
+        override protected void OnEnable()
+        {
+            base.OnEnable();
+            __HighlightedScale = serializedObject.FindProperty(nameof(__HighlightedScale));
+            __ScaleAnimation = serializedObject.FindProperty(nameof(__ScaleAnimation));
+        }
+
+        override public void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(__HighlightedScale);
+            EditorGUILayout.PropertyField(__ScaleAnimation);
+            serializedObject.ApplyModifiedProperties();
+            base.OnInspectorGUI();
+        }
+    }
+}
+#endif
