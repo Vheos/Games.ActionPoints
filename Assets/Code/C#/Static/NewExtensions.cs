49bfa7e708971cf9182c33cf959ce0841ccccdc7
Vheos <Vheos777@gmail.com>
2021-10-06 02:58:28 +0200

    added methods from previous project

--- Assets/Code/C#/Static/NewExtensions.cs
+++ Assets/Code/C#/Static/NewExtensions.cs
@@ -73,5 +73,96 @@ namespace Vheos.Games.ActionPoints
         }
         static public Vector3 CursorToScreenPlanePoint(this Camera t, Vector3 worldPoint)
         => t.CursorToPlanePoint(t.ScreenPlane(worldPoint));
+
+        // Input
+        /// <summary> Checks if this key has just been pressed. </summary>
+        static public bool Pressed(this KeyCode t)
+        => Input.GetKeyDown(t);
+        /// <summary> Checks if this key has just been released. </summary>
+        static public bool Released(this KeyCode t)
+        => Input.GetKeyUp(t);
+        /// <summary> Checks if this key is held down. </summary>
+        static public bool Down(this KeyCode t)
+        => Input.GetKey(t);
+        /// <summary> Checks if this key is resting. </summary>
+        static public bool Up(this KeyCode t)
+        => !Input.GetKey(t);
+
+        // Transform
+        /// <summary> Applies chosen a transform to this direction. </summary>
+        static public Vector3 TransformDirection(this Vector2 t, Transform a)
+        => a.TransformDirection(t);
+        /// <summary> Reverts chosen a transform from this direction. </summary>
+        static public Vector3 UntransformDirection(this Vector2 t, Transform a)
+        => a.InverseTransformDirection(t);
+        /// <summary> Applies chosen a transform to this direction. </summary>
+        static public Vector3 TransformDirection(this Vector3 t, Transform a)
+        => a.TransformDirection(t);
+        /// <summary> Reverts chosen a transform from this direction. </summary>
+        static public Vector3 UntransformDirection(this Vector3 t, Transform a)
+        => a.InverseTransformDirection(t);
+        /// <summary> Returns this vector projected on a plane with normal a. </summary>
+        static public Vector3 ProjectVectorOnPlane(this Vector3 t, Vector3 a)
+        => Vector3.ProjectOnPlane(t, a);
+        static public Vector3 Rotate(this Vector2 t, Vector3 a, float b, bool inDegrees = true)
+        => t.Append().Rotate(a, b, inDegrees);
+        static public Vector3 Rotate(this Vector3 t, Vector3 a, float b, bool inDegrees = true)
+        {
+            if (!inDegrees)
+                b *= Mathf.Deg2Rad;
+
+            return Quaternion.AngleAxis(b, a) * t;
+        }
+
+        // Vector
+        /// <summary> Returns the on-screen distance between this vector and a from camera b's perspective. </summary>
+        static public float ScreenDistanceTo(this Vector3 t, Vector3 a, Camera b)
+        => (b.WorldToScreenPoint(a) - b.WorldToScreenPoint(t)).magnitude;
+        /// <summary> Returns the on-screen offset from this vector to a from camera b's perspective. </summary>
+        static public Vector3 ScreenOffsetTo(this Vector3 t, Vector3 a, Camera b)
+        => (b.WorldToScreenPoint(a) - b.WorldToScreenPoint(t));
+        /// <summary> Returns the on-screen offset from a to this vector from camera b's perspective. </summary>
+        static public Vector3 ScreenOffsetFrom(this Vector3 t, Vector3 a, Camera b)
+        => (b.WorldToScreenPoint(t) - b.WorldToScreenPoint(a));
+        /// <summary> Returns the on-screen direction from this vector towards a from camera b's perspective. </summary>
+        static public Vector3 ScreenDirectionTowards(this Vector3 t, Vector3 a, Camera b)
+        => (b.WorldToScreenPoint(a) - b.WorldToScreenPoint(t)).normalized;
+        /// <summary> Returns the on-screen direction from a towards this vector from camera b's perspective. </summary>
+        static public Vector3 ScreenDirectionAwayFrom(this Vector3 t, Vector3 a, Camera b)
+        => (b.WorldToScreenPoint(t) - b.WorldToScreenPoint(a)).normalized;
+        public static Vector2 PerpendicularCW(this Vector2 t)
+        => new Vector2(t.y, -t.x); 
+        public static Vector2 PerpendicularCCW(this Vector2 t)
+        => new Vector2(-t.y, t.x);
+        
+
+
+        // Edge points
+        /// <summary> Returns a point on this rectangle's edge given direction a. </summary>
+        static public Vector2 EdgePoint(this Rect t, Vector2 a)
+        => t.center + t.size.Div(2f).Mul(a.NormalizeComps());
+        /// <summary> Returns a point on this cuboid's surface given direction a. </summary>
+        static public Vector3 SurfacePoint(this Bounds t, Vector3 a)
+        => t.center + t.extents.Mul(a.NormalizeComps());
+
+        // Bounds
+        static public Bounds LocalBounds(this Collider t)
+        {
+            return t switch
+            {
+                BoxCollider r => new Bounds(r.center, r.size),
+                SphereCollider r => new Bounds(r.center, (2 * r.radius).ToVector3()),
+                CapsuleCollider r => new Bounds(r.center, new Vector3(r.height, 2 * r.radius, 2 * r.radius).Rotate(r.transform.rotation)),
+                MeshCollider r => r.sharedMesh.bounds,
+                _ => default,
+            };
+        }
+        /// <summary> Returns a rectangle equivalent to this cuboid with depth removed. </summary>
+        static public Rect ToRect(this Bounds t)
+        => new Rect
+        {
+            size = t.size,
+            center = t.center,
+        };
     }
 }
--- Assets/Code/C#/Static/NewUtility.cs
+++ Assets/Code/C#/Static/NewUtility.cs
@@ -2,6 +2,7 @@ namespace Vheos.Games.ActionPoints
 {
     using UnityEngine;
     using Tools.Extensions.Math;
+
     static public class NewUtility
     {
         // Various
@@ -21,5 +22,21 @@ namespace Vheos.Games.ActionPoints
 
             return new AnimationCurve(keyframes);
         }
+        static public Vector2 PointOnCircle(float angle, float radius = 1f, bool inDegrees = true)
+        {
+            if (inDegrees)
+                angle *= Mathf.Deg2Rad;
+            return new Vector2(radius * Mathf.Cos(angle), radius * -Mathf.Sin(angle));
+        }
+        static public Vector3 PointOnCircleClosestTo(Vector3 point, Transform circleTransform = null)
+        {
+            if (circleTransform != null)
+                point = point.Untransform(circleTransform);
+            point = point.XY().normalized;
+            if (circleTransform != null)
+                point = point.Transform(circleTransform);
+            return point;
+        }
+
     }
 }
