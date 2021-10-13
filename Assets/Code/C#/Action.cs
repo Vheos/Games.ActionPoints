2524efd1f9bc63886938b41037075f51b148d12c
Vheos <Vheos777@gmail.com>
2021-10-13 03:48:54 +0200

    added methods 'CanBeUsed` and 'Use'

--- Assets/Code/C#/Action.cs
+++ Assets/Code/C#/Action.cs
@@ -3,19 +3,24 @@ namespace Vheos.Games.ActionPoints
     using UnityEngine;
     public class Action : ScriptableObject
     {
-        // Publics
+        // Inspector
         public Sprite Sprite = null;
         [Range(0, 5)] public int ActionPointsCost = 1;
         [Range(0, 5)] public int FocusPointsCost = 1;
         public bool IsTargeted = false;
-        public void Invoke(Character character)
-        {
-            Debug.Log($"{character.name} + {GetType().Name}");
-        }
 
+        // Publics
         public bool CanBeUsed(Character character)
         {
-            return true;
+            return character.ActionProgress >= 0
+                && character.ActionPointsCount >= ActionPointsCost
+                && character.FocusPointsCount >= FocusPointsCost;
+        }
+        public void Use(Character user, Character target)
+        {
+            user.ChangeActionPoints(-ActionPointsCost);
+            user.ChangeFocusPoints(-FocusPointsCost);
+            Debug.Log($"{user.name} used {name} on {target.name}");
         }
     }
 }
