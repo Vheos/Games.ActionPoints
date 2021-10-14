788bb0af3466a2ff0f853467fe9d16b9ce3b3962
Vheos <Vheos777@gmail.com>
2021-10-14 15:31:50 +0200

    added initialization

--- Assets/Code/Components/UI/UICostPoint.cs
+++ Assets/Code/Components/UI/UICostPoint.cs
@@ -1,6 +1,35 @@
 namespace Vheos.Games.ActionPoints
 {
+    using UnityEngine;
+
     public class UICostPoint : AUIPoint
     {
+        // Inspector
+        [Range(0f, 1f)] public float _Opacity = 0.5f;
+
+        // Publics
+        public UICostPointsBar CostPointsBar
+        { get; set; }
+
+        // Mono
+        public override void PlayStart()
+        {
+            base.PlayStart();
+            Opacity = _Opacity;
+            ActionProgress = 1f;
+
+            bool isActionPoint = Index < CostPointsBar.Button.Action.ActionPointsCost;
+            FocusProgress = isActionPoint ? 0f : 1f;
+            Shape = isActionPoint ? UI._PointActionShape : UI._PointFocusShape;
+        }
     }
 }
+
+/*
+// Privates
+public void UpdateColor(int _, int actionPointsCount)
+=> ActionColor = Index < actionPointsCount ? UI._PointActionColor : UI._PointExhaustColor;
+
+UpdateColor(0, UI.Character.ActionPointsCount);
+UI.Character.OnActionPointsCountChanged += UpdateColor;
+*/
