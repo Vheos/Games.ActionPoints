bba66c81b3ce7827e1b3801cca09f9aa055c6981
Vheos <Vheos777@gmail.com>
2021-10-20 07:36:28 +0200

    added support for `ActionScripts`

--- Assets/Code/C#/Action.cs
+++ Assets/Code/C#/Action.cs
@@ -1,25 +1,30 @@
 namespace Vheos.Games.ActionPoints
 {
     using UnityEngine;
+
+    [CreateAssetMenu(fileName = nameof(Action), menuName = nameof(Action))]
     public class Action : ScriptableObject
     {
         // Inspector
-        public Sprite Sprite = null;
-        [Range(0, 5)] public int ActionPointsCost = 1;
-        [Range(0, 5)] public int FocusPointsCost = 1;
-        public bool IsTargeted = false;
+        public Sprite _Sprite = null;
+        [Range(0, 5)] public int _ActionPointsCost = 1;
+        [Range(0, 5)] public int _FocusPointsCost = 1;
+        public bool _IsTargeted = false;
+        public AActionScript[] _ActionScripts = new AActionScript[0];
 
         // Publics
         public bool CanBeUsed(Character character)
         {
-            return character.ActionProgress >= 0
-                && character.FocusPointsCount >= FocusPointsCost;
+            return !character.IsExhausted
+                && character.FocusPointsCount >= _FocusPointsCost;
         }
         public void Use(Character user, Character target)
         {
-            user.ChangeActionPoints(-ActionPointsCost);
-            user.ChangeFocusPoints(-FocusPointsCost);
-            Debug.Log($"{user.name} used {name} on {target.name}");
+            Debug.Log($"{user.name} is using {name}" + (target != null ? $" on {target.name}" : ""));
+            user.ChangeActionPoints(-_ActionPointsCost);
+            user.ChangeFocusPoints(-_FocusPointsCost);
+            foreach (var script in _ActionScripts)
+                script.Invoke(user, target);
         }
     }
 }
