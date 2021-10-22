8f0c2f6f333509930e0e3ac61be2f44911ce7f3b
Vheos <Vheos777@gmail.com>
2021-10-22 05:18:06 +0200

    updated code

--- Assets/Code/Components/UI/UIDamagePopup.cs
+++ Assets/Code/Components/UI/UIDamagePopup.cs
@@ -19,19 +19,22 @@ namespace Vheos.Games.ActionPoints
         public Gradient _ColorCurve;
         [Range(1f, 3f)] public float _WoundPulseScale = 2f;
         [Range(0f, 1f)] public float _WoundPulseDuration = 0.5f;
+        [Range(0f, 1f)] public float _PercentSignSize = 0.5f;
 
         // Publics
         public void Initialize(Vector3 position, float damage, int wounds)
         {
             bool isWound = wounds > 0;
             float lerpAlpha = isWound ? 1f : damage / 100f;
-            _textMesh.fontSize = _SizeCurve.Evaluate(lerpAlpha);
             _textMesh.color = _ColorCurve.Evaluate(lerpAlpha);
-            _textMesh.text = damage.RoundDown().ToString();
-
+            transform.localScale = transform.localScale.Mul(_SizeCurve.Evaluate(lerpAlpha));
             transform.position = position;
             transform.rotation = CameraManager.FirstActive.transform.rotation;
 
+            _textMesh.text = damage.RoundDown().ToString();
+            if (_PercentSignSize > 0f)
+                _textMesh.text += $"<size={_PercentSignSize.ToInvariant("F2")}>%</size>";
+            
             FadeIn();
             if (isWound)
                 Pulse();
@@ -41,9 +44,10 @@ namespace Vheos.Games.ActionPoints
         private TextMeshPro _textMesh;
         private void FadeIn()
         {
-            Vector2 direction = NewUtility.PointOnCircle(_AngleRandomRange.RandomMinMax() - 90f, 1f, true);
+            Vector2 localDirection = NewUtility.PointOnCircle(_AngleRandomRange.RandomMinMax() - 90f, 1f, true);
+            Vector3 direction = localDirection.Rotate(CameraManager.FirstActive.transform.rotation);
             if (_AlignTextRotationToDirection)
-                transform.localRotation = Quaternion.LookRotation(transform.forward, direction);
+                transform.localRotation = Quaternion.LookRotation(transform.forward, localDirection);
             transform.AnimateLocalPosition(this, direction * _Distance, _FadeInDuration);
             _textMesh.AnimateAlpha(this, 1f, _FadeInDuration, false, StayUp);
         }
@@ -52,7 +56,7 @@ namespace Vheos.Games.ActionPoints
         private void FadeOut()
         => _textMesh.AnimateAlpha(this, 0f, _FadeOutDuration, false, () => this.DestroyObject());
         private void Pulse()
-        => transform.AnimateLocalScale(this, _WoundPulseScale.ToVector2(), _WoundPulseDuration, true, Pulse);
+        => transform.AnimateLocalScale(this, transform.localScale * _WoundPulseScale, _WoundPulseDuration, true, Pulse);
 
         // Mono
         public override void PlayAwake()
