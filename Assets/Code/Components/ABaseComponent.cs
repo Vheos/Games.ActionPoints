5799890cfe44570cef64c4d38622ed969f21315c
Vheos <Vheos777@gmail.com>
2021-11-02 15:12:31 +0100

    reworked components cache into a component itself

--- Assets/Code/Components/ABaseComponent.cs
+++ Assets/Code/Components/ABaseComponent.cs
@@ -14,48 +14,19 @@ namespace Vheos.Games.ActionPoints
 #endif
     {
         // Publics
-        public T Get<T>() where T : Component
-        => (T)_cachedComponentsByTypeByGameObject[gameObject][typeof(T)];
         public Updatable Updatable
         => GetComponentOrThrowWarning<Updatable>();
-        public Mousable Mousable
-        => GetComponentOrThrowWarning<Mousable>();
-        public SpriteChangable SpriteChangable
-        => GetComponentOrThrowWarning<SpriteChangable>();
-        public Movable Movable
-        => GetComponentOrThrowWarning<Movable>();
+        public T Get<T>() where T : Component
+        => _componentCache.Get<T>();
+        public void AddToCache<T>() where T : Component
+        => _componentCache.Add<T>();
 
         // Privates
+        private ComponentCache _componentCache;
         virtual protected void SubscribeToPlayEvents()
         { }
-        virtual protected Type[] ComponentsTypesToCache
-        => null;
-        static private Dictionary<GameObject, Dictionary<Type, Component>> _cachedComponentsByTypeByGameObject;
-        private void TryCacheComponents()
-        {
-            if (!ComponentsTypesToCache.TryNonNull(out var componentTypesToCache))
-                return;
-
-            if (!_cachedComponentsByTypeByGameObject.ContainsKey(gameObject))
-                _cachedComponentsByTypeByGameObject[gameObject] = new Dictionary<Type, Component>();
-
-            foreach (var type in componentTypesToCache)
-            {
-                if (_cachedComponentsByTypeByGameObject[gameObject].ContainsKey(type))
-                    continue;
-                if (!type.IsAssignableTo<Component>())
-                {
-                    WarningWrongType(type);
-                    continue;
-                }
-                if (!TryGetComponent(type, out var component))
-                {
-                    WarningComponentNotFound(type);
-                    continue;
-                }
-                _cachedComponentsByTypeByGameObject[gameObject].Add(type, component);
-            }
-        }
+        virtual protected void AddToComponentCache()
+        { }
         private T GetComponentOrThrowWarning<T>() where T : Component
         {
             T r = GetComponent<T>();
@@ -67,57 +38,15 @@ namespace Vheos.Games.ActionPoints
         // Warnings
         private void WarningEventHandlerNotFound<T>()
         => Debug.LogWarning($"{nameof(ABaseComponent)} / EventHandlerNotFound   -   gameobject {name}, component {GetType().Name}, type {typeof(T)}");
-        private void WarningWrongType(Type type)
-        => Debug.LogWarning($"{nameof(ABaseComponent)} / WrongType   -   gameobject {name}, component {GetType().Name}, type {type}");
-        private void WarningComponentNotFound(Type type)
-        => Debug.LogWarning($"{nameof(ABaseComponent)} / ComopnentNotFound   -   gameobject {name}, component {GetType().Name}, type {type}");
-
-        // Initializers
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-        static private void StaticInitialize()
-        => _cachedComponentsByTypeByGameObject = new Dictionary<GameObject, Dictionary<Type, Component>>();
 
         // Play
         public override void PlayAwake()
         {
             base.PlayAwake();
-            TryCacheComponents();
+            _componentCache = gameObject.AddComponent<ComponentCache>();
+            AddToComponentCache();
             SubscribeToPlayEvents();
         }
-
-#if UNITY_EDITOR
-        // Debug
-        [ContextMenu(nameof(PrintLocalCache))]
-        public void PrintLocalCache()
-        {
-            int localCachedCount = _cachedComponentsByTypeByGameObject[gameObject].Count;
-            int localAllCount = GetComponents<Component>().Length - 1;
-            Debug.Log($"{name.ToUpper()} ({localCachedCount}/{localAllCount})");
-            foreach (var componentByType in _cachedComponentsByTypeByGameObject[gameObject])
-                Debug.Log($"\t- {componentByType.Key.Name}");
-            Debug.Log($"");
-        }
-        [ContextMenu(nameof(PrintGlobalCache))]
-        public void PrintGlobalCache()
-        {
-            int cachedCount = 0;
-            int allCount = 0;
-            foreach (var componentByTypeByGameObject in _cachedComponentsByTypeByGameObject)
-            {
-                int localCachedCount = componentByTypeByGameObject.Value.Count;
-                int localAllCount = componentByTypeByGameObject.Key.GetComponents<Component>().Length - 1;
-                Debug.Log($"{componentByTypeByGameObject.Key.name.ToUpper()} ({localCachedCount}/{localAllCount})");
-                foreach (var componentByType in componentByTypeByGameObject.Value)
-                    Debug.Log($"\t- {componentByType.Key.Name}");
-                Debug.Log($"");
-                cachedCount += localCachedCount;
-                allCount += localAllCount;
-            }
-            Debug.Log($"Cached GameObjects: {_cachedComponentsByTypeByGameObject.Count}");
-            Debug.Log($"Cached Components: {cachedCount}");
-            Debug.Log($"All Components: {allCount}");
-            Debug.Log($"");
-        }
-#endif
     }
 }
+
