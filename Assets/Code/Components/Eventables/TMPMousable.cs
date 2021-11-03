10a1df8f49843e3cd35c74a3cfdd6201d972a291
Vheos <Vheos777@gmail.com>
2021-11-03 14:56:06 +0100

    added mousable for `TextMeshPro`, to auto-resize trigger on text change

---
+++ Assets/Code/Components/Eventables/TMPMousable.cs
@@ -0,0 +1,31 @@
+namespace Vheos.Games.ActionPoints
+{
+    using TMPro;
+    using UnityEngine;
+    using Vheos.Tools.Extensions.General;
+    using Vheos.Tools.Extensions.UnityObjects;
+
+    sealed public class TMPMousable : Mousable
+    {
+        // Privates
+        private TextMeshPro _tmp;
+        private void TryFitBoxColliderOnTextChanged(Object tmp)
+        {
+            if (tmp == _tmp)
+                TryFitBoxColliderToMesh();
+        }
+
+        // Play
+        public override void PlayAwake()
+        {
+            base.PlayAwake();
+            _tmp = GetComponent<TextMeshPro>();
+            TMPro_EventManager.TEXT_CHANGED_EVENT.Add(TryFitBoxColliderOnTextChanged);
+        }
+        public override void PlayDestroy()
+        {
+            base.PlayDestroy();
+            TMPro_EventManager.TEXT_CHANGED_EVENT.Remove(TryFitBoxColliderOnTextChanged);
+        }
+    }
+}
