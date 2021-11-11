a7427d719185c920ef86261e9423eeb3c94b349b
Vheos <Vheos777@gmail.com>
2021-11-11 01:17:44 +0100

    bugfix: bool's `ToSign` was returning reversed values

--- Assets/Code/Static/NewExtensions.cs
+++ Assets/Code/Static/NewExtensions.cs
@@ -142,7 +142,7 @@ namespace Vheos.Games.ActionPoints
         static public int To01(this bool t)
         => t ? 1 : 0;
         static public int ToSign(this bool t)
-        => t ? -1 : +1;
+        => t ? +1 : -1;
         static public int Map(this bool t, int a, int b)
         => t ? a : b;
         static public float Map(this bool t, float a, float b)
