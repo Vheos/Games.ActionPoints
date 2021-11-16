74e24f0b1a1e66bc2acba749bc2e61a5f7a61f88
Vheos <Vheos777@gmail.com>
2021-11-16 06:40:01 +0100

    added allies/enemies-related methods, added `enabled` handling

--- Assets/Code/Components/EventHandlers/Combatable.cs
+++ Assets/Code/Components/EventHandlers/Combatable.cs
@@ -4,6 +4,7 @@ namespace Vheos.Games.ActionPoints
     using UnityEngine;
     using Tools.UnityCore;
     using Tools.Extensions.Math;
+    using System.Linq;
 
     public class Combatable : ABaseComponent
     {
@@ -16,11 +17,73 @@ namespace Vheos.Games.ActionPoints
         { get; private set; }
         public Vector3 AnchorPosition
         { get; private set; }
+        public IEnumerable<Combatable> Allies
+        {
+            get
+            {
+                if (Combat == null
+                || !TryGetComponent<Teamable>(out var teamable))
+                    yield break;
+
+                foreach (var combatable in Combat.Members)
+                    if (combatable.TryGetComponent<Teamable>(out var combatTeamable)
+                    && teamable.IsAlliedWith(combatTeamable))
+                        yield return combatable;
+            }
+        }
+        public IEnumerable<Combatable> Enemies
+        {
+            get
+            {
+                if (Combat == null)
+                    yield break;
+
+                Teamable teamable = Get<Teamable>();
+                foreach (var combatable in Combat.Members)
+                    if (teamable != null
+                    && combatable.TryGetComponent<Teamable>(out var combatTeamable)
+                    && !teamable.IsAlliedWith(combatTeamable)
+                    || teamable == null
+                    && this != combatable)
+                        yield return combatable;
+            }
+        }
+        public Vector3 AllyMidpoint
+        {
+            get
+            {
+                Vector3 r = Vector3.zero;
+                int count = 0;
+                foreach (var ally in Allies)
+                {
+                    r += ally.transform.position;
+                    count++;
+                }
+                return r / count;
+            }
+        }
+        public Vector3 EnemyMidpoint
+        {
+            get
+            {
+                Vector3 r = Vector3.zero;
+                int count = 0;
+                foreach (var enemy in Enemies)
+                {
+                    r += enemy.transform.position;
+                    count++;
+                }
+                return r / count;
+            }
+        }
         public bool IsInCombat
         => Combat != null;
-        public void StartCombatWith(Combatable target)
+        public bool IsInCombatWith(Combatable other)
+        => this != other && Combat == other.Combat;
+        public void TryStartCombatWith(Combatable target)
         {
             if (this.IsInCombat && target.IsInCombat
+            || !this.enabled || !target.enabled
             || this == target)
                 return;
 
@@ -35,9 +98,12 @@ namespace Vheos.Games.ActionPoints
             else
                 this.JoinCombat(target.Combat);
         }
-        public void LeaveCombat()
+        public void TryLeaveCombat()
         {
-            Combat.RemoveMember(this);
+            if (Combat == null)
+                return;
+
+            Combat.TryRemoveMember(this);
             Combat = null;
             OnCombatChanged?.Invoke(null);
         }
@@ -46,7 +112,7 @@ namespace Vheos.Games.ActionPoints
         private void JoinCombat(Combat combat)
         {
             Combat = combat;
-            combat.AddMember(this);
+            combat.TryAddMember(this);
             AnchorPosition = transform.position;
             OnCombatChanged?.Invoke(combat);
         }
