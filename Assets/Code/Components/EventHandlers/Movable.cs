8fe126adac10a4d0a0ef4b05aec6529fe53421f4
Vheos <Vheos777@gmail.com>
2021-11-16 06:35:23 +0100

    bugfix: event `OnStopped` was being invoked every frame when not moving

--- Assets/Code/Components/EventHandlers/Movable.cs
+++ Assets/Code/Components/EventHandlers/Movable.cs
@@ -2,33 +2,51 @@ namespace Vheos.Games.ActionPoints
 {
     using UnityEngine;
     using Tools.UnityCore;
+    using Event = Tools.UnityCore.Event;
 
     [DisallowMultipleComponent]
     sealed public class Movable : AEventSubscriber
     {
         // Events
+        public Event OnStartedMoving
+        { get; } = new Event();
         public Event<Vector3, Vector3> OnMoved
         { get; } = new Event<Vector3, Vector3>();
-        public Event<Vector3> OnStopped
-        { get; } = new Event<Vector3>();
+        public Event OnStoppedMoving
+        { get; } = new Event();
 
         // Privates
         private Vector3 _previousPosition;
+        private bool _previousHasMoved;
         private void TryInvokeEvents()
         {
             Vector3 currentPosition = transform.position;
-            if (_previousPosition != currentPosition)
+            bool currentHasMoved = currentPosition != _previousPosition;
+
+            if (currentHasMoved)
+            {
+                if (!_previousHasMoved)
+                    OnStartedMoving?.Invoke();
                 OnMoved?.Invoke(_previousPosition, currentPosition);
-            else
-                OnStopped?.Invoke(currentPosition);
+            }
+            else if (_previousHasMoved)
+                OnStoppedMoving?.Invoke();
+
             _previousPosition = currentPosition;
+            _previousHasMoved = currentHasMoved;
         }
 
         // Play
-        protected override void SubscribeToEvents()
+        protected override void AutoSubscribeToEvents()
         {
-            base.SubscribeToEvents();
-            SubscribeTo(GetHandler<Updatable>().OnUpdated, TryInvokeEvents);
+            base.AutoSubscribeToEvents();
+            SubscribeTo(Get<Updatable>().OnUpdated, TryInvokeEvents);
+        }
+        protected override void PlayEnable()
+        {
+            base.PlayEnable();
+            _previousPosition = transform.position;
+            _previousHasMoved = false;
         }
     }
 }
