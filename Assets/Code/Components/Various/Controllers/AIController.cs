bd9254428c4a386c5ce79ef20c8c5a341aa5aa8b
Vheos <Vheos777@gmail.com>
2021-11-18 01:40:46 +0100

    started implementing

--- Assets/Code/Components/Various/Controllers/AIController.cs
+++ Assets/Code/Components/Various/Controllers/AIController.cs
@@ -6,6 +6,22 @@ namespace Vheos.Games.ActionPoints
 
     public class AIController : AController
     {
+        // Inspector
+        [SerializeField] protected Action _Action;
+        [SerializeField] [Range(0f, 1f)] protected float _TargetingTime;
 
+        // Private
+        private void OnUpdate()
+        {
+            Combatable combatable = Get<Combatable>();
+            
+        }
+
+        // Play
+        protected override void DefineAutoSubscriptions()
+        {
+            base.DefineAutoSubscriptions();
+            SubscribeTo(Get<Updatable>().OnUpdated, OnUpdate);
+        }
     }
 }
