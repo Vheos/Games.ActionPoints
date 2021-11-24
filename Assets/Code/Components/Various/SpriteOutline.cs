9c9e6565eb71e3e17dffa927348f9116319f7369
Vheos <Vheos777@gmail.com>
2021-11-24 06:52:05 +0100

    moved most settings to `UIManager`, made independent from `SpriteChangeable`

--- Assets/Code/Components/Various/SpriteOutline.cs
+++ Assets/Code/Components/Various/SpriteOutline.cs
@@ -14,13 +14,12 @@ namespace Vheos.Games.ActionPoints
         private const string TEXTURE_MPROP_NAME = "_MainTex";
 
         // Inspector
-        [SerializeField] protected Material _Material = null;
-        [SerializeField] [Range(0f, 1f)] protected float _Thickness = 0.5f;
         [SerializeField] protected Color _Color = Color.white;
-        [SerializeField] [Range(0f, 1f)] protected float _FadeInDuration = 0.5f;
-        [SerializeField] [Range(0f, 1f)] protected float _FadeOutDuration = 0.5f;
+        [SerializeField] [Range(0f, 1f)] protected float _Thickness = 0.5f;
 
         // Public
+        public UISettings.SpriteOutlineSettings Settings
+        => UIManager.Settings.SpriteOutline;
         public float Thickness
         {
             get => GetFloat(nameof(Thickness));
@@ -34,7 +33,7 @@ namespace Vheos.Games.ActionPoints
         public void Show()
         {
             _outlineRenderer.GOActivate();
-            using (QAnimator.Group(this, null, _FadeInDuration))
+            using (QAnimator.Group(this, null, Settings.FadeInDuration))
             {
                 QAnimator.GroupAnimate(v => Thickness = v, Thickness, _Thickness);
                 _outlineRenderer.GroupAnimateColor(_Color);
@@ -42,7 +41,7 @@ namespace Vheos.Games.ActionPoints
         }
         public void Hide(bool instantly = false)
         {
-            using (QAnimator.Group(this, null, instantly ? 0f : _FadeOutDuration, _outlineRenderer.GODeactivate))
+            using (QAnimator.Group(this, null, instantly ? 0f : Settings.FadeOutDuration, _outlineRenderer.GODeactivate))
             {
                 QAnimator.GroupAnimate(v => Thickness = v, Thickness, 0f);
                 _outlineRenderer.GroupAnimateAlpha(0f);
@@ -61,7 +60,8 @@ namespace Vheos.Games.ActionPoints
         protected override void DefineAutoSubscriptions()
         {
             base.DefineAutoSubscriptions();
-            SubscribeTo(Get<SpriteChangable>().OnChangeSprite, UpdateOutlineSprite);
+            if (TryGet<SpriteChangable>(out var spriteChangable))
+                SubscribeTo(spriteChangable.OnChangeSprite, UpdateOutlineSprite);
         }
         protected override void InitializeRenderer(out Renderer renderer)
         {
@@ -71,8 +71,8 @@ namespace Vheos.Games.ActionPoints
         protected override void PlayAwake()
         {
             base.PlayAwake();
-            _outlineRenderer.sharedMaterial = _Material;
-            _outlineRenderer.sprite = Get<SpriteRenderer>().sprite;
+            _outlineRenderer.sharedMaterial = Settings.Material;
+            UpdateOutlineSprite(null, Get<SpriteRenderer>().sprite);
             Hide(true);
         }
     }
