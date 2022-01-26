d093a8d51beba2ae321f07d4e02f34ab8aa72563
Vheos <Vheos777@gmail.com>
2022-01-26 04:53:52 +0100

    cleaned up

--- Assets/Code/Runtime/Managers/CameraManager.cs
+++ Assets/Code/Runtime/Managers/CameraManager.cs
@@ -3,16 +3,11 @@ namespace Vheos.Games.ActionPoints
     using System;
     using UnityEngine;
     using Games.Core;
-    using Tools.Extensions.Collections;
     using System.Linq;
-    using Vheos.Tools.Extensions.General;
 
     [DisallowMultipleComponent]
     public class CameraManager : AComponentManager<CameraManager, Camera>
     {
-        // Inspector
-        [SerializeField] protected Camera _UICameraPrefab;
-
         // Publics
         static public Camera AnyNonUI
         => _components.FirstOrDefault(t => !t.IsOnLayer(BuiltInLayer.UI));
