ac34eb96781d00549ab0acbc040f848e85da30d0
Vheos <Vheos777@gmail.com>
2022-01-28 03:15:54 +0100

    exported `LerpHalfTimeToAlpha` to `Games.Core`

--- Assets/Code/Runtime/NewUtility.cs
+++ Assets/Code/Runtime/NewUtility.cs
@@ -124,14 +124,6 @@ namespace Vheos.Games.ActionPoints
 
 
         // UTILITY
-        static public float LerpHalfTimeToAlpha(float lerpHalfTime)
-        {
-            if (lerpHalfTime == 0f)
-                return 1f;
-
-            float deltaTime = Time.inFixedTimeStep ? Time.fixedDeltaTime : Time.deltaTime;
-            return 1f - 0.5f.Pow(deltaTime / lerpHalfTime);
-        }
         static public Vector2 PointOnCircle(float angle, float radius = 1f, bool inDegrees = true)
         {
             if (inDegrees)
