63c9eb8f311bb637663171edfb517ce14e7af325
Vheos <Vheos777@gmail.com>
2022-01-28 03:13:21 +0100

    removed shared properties

--- Assets/Code/Runtime/MProps/ActionPointMProps.cs
+++ Assets/Code/Runtime/MProps/ActionPointMProps.cs
@@ -6,32 +6,11 @@ namespace Vheos.Games.ActionPoints
 
     sealed public class ActionPointMProps : AAutoMProps
     {
-        // Pubilcs
-        public void AnimateOpacity(float to, float duration)
-        => Tween.New
-            .SetDuration(duration)
-            .AddPropertyModifier(v => Opacity += v, to - Opacity);
-
         // MProps
         public Texture Shape
         {
             get => GetTexture(nameof(Shape));
             set => SetTexture(nameof(Shape), value);
-        }
-        public Color BackgroundColor
-        {
-            get => GetColor(nameof(BackgroundColor));
-            set => SetColor(nameof(BackgroundColor), value);
-        }
-        public Color ActionColor
-        {
-            get => GetColor(nameof(ActionColor));
-            set => SetColor(nameof(ActionColor), value);
-        }
-        public Color FocusColor
-        {
-            get => GetColor(nameof(FocusColor));
-            set => SetColor(nameof(FocusColor), value);
         }
         public float Opacity
         {
@@ -47,18 +26,6 @@ namespace Vheos.Games.ActionPoints
         {
             get => GetFloat(nameof(FocusProgress));
             set => SetFloat(nameof(FocusProgress), value);
-        }
-
-        // Defines
-        private enum MProp
-        {
-            Shape,
-            ColorA,  // Focus
-            ColorB,  // Action
-            ColorC,  // Background
-            ThresholdA,  // Focus
-            ThresholdB,  // Action
-            Opacity,
         }
     }
 }
