c632f1516ec4593d47bf04557fb1e515eed35546
Vheos <Vheos777@gmail.com>
2022-02-03 17:36:48 +0100

    added button visuals initialization

--- Assets/Code/Runtime/ActionUI/ActionButton.cs
+++ Assets/Code/Runtime/ActionUI/ActionButton.cs
@@ -7,6 +7,7 @@ namespace Vheos.Games.ActionPoints
     using Tools.Extensions.Math;
     using Tools.Extensions.UnityObjects;
     using Tools.Utilities;
+    using TMPro;
 
     [RequireComponent(typeof(Raycastable))]
     [RequireComponent(typeof(Selectable))]
@@ -17,17 +18,46 @@ namespace Vheos.Games.ActionPoints
         [SerializeField] [Range(0.1f, 1f)] protected float _Radius;
 
         // Publics
-        public ActionButtonsWheel Wheel
-        { get; private set; }
         public float Radius
         => _Radius;
+        public ActionButtonsWheel Wheel
+        { get; private set; }
+        public Action Action
+        { get; private set; }
 
         // Play
-        public void Initialize(ActionButtonsWheel wheel, int index)
+        public void Initialize(ActionButtonsWheel wheel, int index, Action action)
         {
             Wheel = wheel;
+            Action = action;
             name = $"Button{index + 1}";
             BindEnableDisable(wheel);
+
+            if (action.ButtonVisuals.Sprite != null)
+            {
+                var spriteRenderer = Add<SpriteRenderer>();
+                spriteRenderer.sprite = action.ButtonVisuals.Sprite;
+                spriteRenderer.color = action.ButtonVisuals.Color;
+                _visualComponent = spriteRenderer;
+            }
+            else if (action.ButtonVisuals.Text != null)
+            {
+                var textMeshPro = Add<TextMeshPro>();
+                textMeshPro.rectTransform.sizeDelta = _Radius.ToVector2();
+                textMeshPro.text = action.ButtonVisuals.Text;
+                textMeshPro.color = action.ButtonVisuals.Color;
+                textMeshPro.transform.SetLocalScaleY(1.5f);
+                textMeshPro.fontStyle = FontStyles.Bold;
+                textMeshPro.enableAutoSizing = true;
+                textMeshPro.fontSizeMin = 0f;
+                textMeshPro.horizontalAlignment = HorizontalAlignmentOptions.Center;
+                textMeshPro.verticalAlignment = VerticalAlignmentOptions.Middle;
+                _visualComponent = textMeshPro;
+            }
+
+            Get<Raycastable>().RaycastTarget = _visualComponent;
+            if (TryGet(out SelectableButtonVisuals selectableButtonVisuals))
+                selectableButtonVisuals.UpdateColorComponentType();
         }
         protected override void PlayEnable()
         {
@@ -41,5 +71,8 @@ namespace Vheos.Games.ActionPoints
             Get<Raycastable>().Disable();
             Get<Selectable>().Disable();
         }
+
+        // Privates
+        private Component _visualComponent;
     }
 }
