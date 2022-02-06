0be943edbcfc5d271f19d612920c0b531c7d348b
Vheos <Vheos777@gmail.com>
2022-02-06 07:20:55 +0100

    added `Camera` wrapper

---
+++ Assets/Code/Runtime/CCamera.cs
@@ -0,0 +1,11 @@
+namespace Vheos.Games.ActionPoints
+{
+    using System;
+    using UnityEngine;
+    using Games.Core;
+
+    [RequireComponent(typeof(Camera))]
+    public class CCamera : AUnityComponentWrapper<Camera>
+    {
+    }
+}
