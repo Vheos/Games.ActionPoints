b4b34d75db7376059a1b72c6ef38384b0871f67c
Vheos <Vheos777@gmail.com>
2022-02-09 08:32:33 +0100

    updated a lot

--- Assets/Code/Runtime/Settings/VisualSettings.cs
+++ Assets/Code/Runtime/Settings/VisualSettings.cs
@@ -10,14 +10,104 @@ namespace Vheos.Games.ActionPoints
         // Constants
         protected const string CONTEXT_MENU_PATH = "Settings/";
 
-        // Inspector
-        [Range(0f, 1f)]  public float TestFloat;
-        public ActionButtonVisualSettings ActionButton;
-
+        // Publics
+        [field: SerializeField] public GeneralSettings General { get; private set; }
+        #region struct
         [Serializable]
-        public struct ActionButtonVisualSettings
+        public struct GeneralSettings
         {
-            [Range(0f, 1f)] public float TestFloat;
+            public Mesh SpriteMesh;
+            public Material ShadowSpriteMaterial;
         }
+        #endregion
+        [field: SerializeField] public UICursorSettings UICursor { get; private set; }
+        #region struct
+        [Serializable]
+        public struct UICursorSettings
+        {
+            public Sprite IdleSprite;
+            public Sprite PressSprite;
+            [Range(0.5f, 2f)] public float PressScale;
+            [Range(0.5f, 2f)] public float PressColorScale;
+            [Range(0f, 1f)] public float PressDuration;
+            [Range(0f, 1f)] public float ReleaseDuration;
+        }
+        #endregion
+        [field: SerializeField] public UITargetingLineSettings UITargetingLine { get; private set; }
+        #region struct
+        [Serializable]
+        public struct UITargetingLineSettings
+        {
+            [Range(0f, 1f)] public float StartOpacity;
+            [Range(0f, 1f)] public float StartWidth;
+            [Range(0f, 1f)] public float EndOpacity;
+            [Range(0f, 1f)] public float EndWidth;
+            [Range(1f, 100f)] public float Tiling;
+            [Range(0f, 1f)] public float ExpandDuration;
+            [Range(0f, 1f)] public float CollapseDuration;
+        }
+        #endregion
+        [field: SerializeField] public CommonSelectableSettings CommonSelectable { get; private set; }
+        #region struct
+        [Serializable]
+        public struct CommonSelectableSettings
+        {
+            [Range(0.5f, 2f)] public float HighlightScale;
+            [Range(0.5f, 2f)] public float HighlightColorScale;
+            [Range(0.5f, 2f)] public float PressScale;
+            [Range(0.5f, 2f)] public float PressColorScale;
+            [Range(0f, 1f)] public float GainHighlightDuration;
+            [Range(0f, 1f)] public float LoseHighlightDuration;
+            [Range(0f, 1f)] public float PressDuration;
+            [Range(0f, 1f)] public float ReleaseDuration;
+        }
+        #endregion
+        [field: SerializeField] public SpriteOutlineSettings SpriteOutline { get; private set; }
+        #region struct
+        [Serializable]
+        public struct SpriteOutlineSettings
+        {
+            public Material Material;
+            [Range(0f, 1f)] public float Thickness;
+            [Range(0f, 1f)] public float ExpandDuration;
+            [Range(0f, 1f)] public float CollapseDuration;
+        }
+        #endregion
+        [field: SerializeField] public ActionUIElementSettings ActionUIElement { get; private set; }
+        #region struct
+        [Serializable]
+        public struct ActionUIElementSettings
+        {
+            [Range(0f, 1f)] public float CreateElementDuration;
+            [Range(0f, 1f)] public float DestroyElementDuration;
+            [Range(0f, 1f)] public float MoveElementDuration;
+            [Range(0f, 1f)] public float ExpandGroupDuration;
+            [Range(0f, 1f)] public float CollapseGroupDuration;
+        }
+        #endregion
+        [field: SerializeField] public ActionPointSettings ActionPoint { get; private set; }
+        #region struct
+        [Serializable]
+        public struct ActionPointSettings
+        {
+            public Material Material;
+            public Texture2D ActionShape;
+            public Texture2D FocusShape;
+            [Range(0f, 1f)] public float PartialOpacity;
+            [Range(0f, 1f)] public float FullOpacity;
+            [Range(0f, 1f)] public float ChangeOpacityDuration;
+            [Range(0f, 1f)] public float ChangeShapeDuration;
+            [Range(-0.25f, +0.25f)] public float Spacing;
+            [Range(0f, 1f)] public float VisualProgressHalfTime;
+        }
+        #endregion
+        [field: SerializeField] public ActionButtonSettings ActionButton { get; private set; }
+        #region struct
+        [Serializable]
+        public struct ActionButtonSettings
+        {
+            [Range(0f, 1f)] public float Radius;
+        }
+        #endregion
     }
 }
