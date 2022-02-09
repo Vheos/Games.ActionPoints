1c2cd4db0bc2e56384fe8a06f483ed9ab24c434c
Vheos <Vheos777@gmail.com>
2022-02-09 13:00:39 +0100

    updated

--- Assets/Code/Runtime/ActionUI/ActionButton.cs
+++ Assets/Code/Runtime/ActionUI/ActionButton.cs
@@ -10,12 +10,78 @@ namespace Vheos.Games.ActionPoints
 
     [RequireComponent(typeof(Raycastable))]
     [RequireComponent(typeof(Selectable))]
+    [RequireComponent(typeof(Targeter))]
+    [RequireComponent(typeof(CommonSelectable))]
     [DisallowMultipleComponent]
     public class ActionButton : AActionUIElement<ActionButtonsWheel>
     {
         // Publics
         public Action Action
         { get; private set; }
+
+        // Privates
+        private bool _isUsable;
+        private ColorComponentType _colorComponentType;
+        private SpriteRenderer CreateSpriteRenderer()
+        {
+            var spriteRenderer = Add<SpriteRenderer>();
+            spriteRenderer.sprite = Action.ButtonVisuals.Sprite;
+            spriteRenderer.color = Action.ButtonVisuals.Color;
+            return spriteRenderer;
+        }
+        private TextMeshPro CreateTextMeshPro()
+        {
+            var textMeshPro = Add<TextMeshPro>();
+            textMeshPro.rectTransform.sizeDelta = Vector2.one.Div(1f, 1.5f);
+            textMeshPro.text = Action.ButtonVisuals.Text.ChooseIf(t => t.IsNotNullOrEmpty(), Action.name.SplitCamelCase().ToUpper());
+            textMeshPro.color = Action.ButtonVisuals.Color;
+            textMeshPro.fontStyle = FontStyles.Bold;
+            textMeshPro.enableAutoSizing = true;
+            textMeshPro.fontSizeMin = 0f;
+            textMeshPro.horizontalAlignment = HorizontalAlignmentOptions.Center;
+            textMeshPro.verticalAlignment = VerticalAlignmentOptions.Middle;
+            return textMeshPro;
+        }
+        private void Selectable_OnPress(Selecter selecter)
+        {
+            if (!_isUsable)
+                return;
+
+            selecter.Get<Player>().TargetingLine.Show(Get<Targeter>(), transform);
+        }
+        private void Selectable_OnRelease(Selecter selecter, bool isFullClick)
+        {
+            if (!_isUsable)
+                return;
+
+            selecter.Get<Player>().TargetingLine.Hide();
+        }
+        private void UpdateUsability(bool instantly = false)
+        {
+            bool previousIsUsable = _isUsable;
+            _isUsable = _group.UI.Actionable.CanAfford(Action);
+            if (_isUsable == previousIsUsable)
+                return;
+
+            AnimateUsability(_isUsable, instantly);
+            if (_isUsable)
+                Get<Selectable>().ClearSelectionAndHolder();
+            Get<CommonSelectable>().IsEnabled = _isUsable;
+           // if(!currentIsUsable)
+
+            previousIsUsable = _isUsable;
+        }
+        private void AnimateUsability(bool state, bool instantly = false)
+        {
+            float targetColorScale = this.Settings().UnusableColorScale;
+            if (state)
+                targetColorScale.SetInv();
+
+            this.NewTween()
+              .SetDuration(this.Settings().ChangeUsabilityDuration)
+              .ColorRatio(_colorComponentType, targetColorScale)
+              .FinishIf(instantly);
+        }
 
         // Play
         public void Initialize(ActionButtonsWheel wheel, Action action)
@@ -26,35 +92,33 @@ namespace Vheos.Games.ActionPoints
             BindEnableDisable(wheel);
             Get<Raycastable>().BindEnableDisable(this);
             Get<Selectable>().BindEnableDisable(this);
-
+            
             _originalScale = this.Settings().Radius.Mul(2).ToVector3();
-            Component _visualComponent = null;
             if (action.ButtonVisuals.Sprite != null)
             {
-                var spriteRenderer = Add<SpriteRenderer>();
-                _visualComponent = spriteRenderer;
-                spriteRenderer.sprite = action.ButtonVisuals.Sprite;
-                spriteRenderer.color = action.ButtonVisuals.Color;
-
+                Get<Raycastable>().RaycastTarget = CreateSpriteRenderer();
+                _colorComponentType = ColorComponentType.SpriteRenderer;
             }
             else
             {
-                var textMeshPro = Add<TextMeshPro>();
-                _visualComponent = textMeshPro;
+                Get<Raycastable>().RaycastTarget = CreateTextMeshPro();
+                _colorComponentType = ColorComponentType.TextMeshPro;
                 _originalScale = _originalScale.Mul(1f, 1.5f, 1f);
-                textMeshPro.rectTransform.sizeDelta = Vector2.one.Div(1f, 1.5f);
-                textMeshPro.text = action.ButtonVisuals.Text.ChooseIf(t => t.IsNotNullOrEmpty(), action.name.SplitCamelCase().ToUpper());
-                textMeshPro.color = action.ButtonVisuals.Color;
-                textMeshPro.fontStyle = FontStyles.Bold;
-                textMeshPro.enableAutoSizing = true;
-                textMeshPro.fontSizeMin = 0f;
-                textMeshPro.horizontalAlignment = HorizontalAlignmentOptions.Center;
-                textMeshPro.verticalAlignment = VerticalAlignmentOptions.Middle;
             }
 
-            Get<Raycastable>().RaycastTarget = _visualComponent;
-            if (TryGet(out CommonSelectable selectableButtonVisuals))
-                selectableButtonVisuals.UpdateColorComponentType();
+            Get<CommonSelectable>().HighlightScale.Set(() => this.Settings().HighlightScale);
+            Get<CommonSelectable>().PressScale.Set(() => this.Settings().PressScale);
+            Get<CommonSelectable>().UpdateColorComponentType();
+
+            OnPlayEnable.SubDestroy(this, () => UpdateUsability());
+            _group.UI.Actionable.OnChangeActionPoints.SubEnableDisable(this, (from, to) => UpdateUsability());
+            _group.UI.Actionable.OnChangeFocusPoints.SubEnableDisable(this, (from, to) => UpdateUsability());
+            _group.UI.Actionable.OnChangeExhausted.SubEnableDisable(this, isExhausted => UpdateUsability());
+            _isUsable = true;
+            UpdateUsability(true);
+
+            Get<Selectable>().OnPress.SubEnableDisable(this, Selectable_OnPress);
+            Get<Selectable>().OnRelease.SubEnableDisable(this, Selectable_OnRelease);
         }
     }
 }
