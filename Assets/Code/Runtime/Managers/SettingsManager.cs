ffae49bca5943fcfb7ff95b7b29436722955ba3c
Vheos <Vheos777@gmail.com>
2022-02-09 08:31:14 +0100

    removed shortcut class `Settings`

--- Assets/Code/Runtime/Managers/SettingsManager.cs
+++ Assets/Code/Runtime/Managers/SettingsManager.cs
@@ -11,20 +11,17 @@ namespace Vheos.Games.ActionPoints
         [field: SerializeField] public VisualSettings VisualSettings { get; private set; }
         [field: SerializeField] public PrefabSettings PrefabSettings { get; private set; }
 
+        // Publics
+        static public VisualSettings Visual
+        => Instance.VisualSettings;
+        static public PrefabSettings Prefabs
+        => Instance.PrefabSettings;
+
         // Play
         protected override void PlayAwake()
         {
             base.PlayAwake();
             Tween.DefaultCurve = Qurve.ValuesByProgress;
         }
-    }
-
-    static public class Settings
-    {
-        // Publics
-        static public VisualSettings Visual
-        => SettingsManager.Instance.VisualSettings;
-        static public PrefabSettings Prefabs
-        => SettingsManager.Instance.PrefabSettings;
     }
 }
