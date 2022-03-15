26e328c3066d25c95ab82128aebade36c135ad3c
Vheos <Vheos777@gmail.com>
2022-03-15 21:20:10 +0100

    added action-related enums and and expanded `ActionPhase`

--- Assets/Code/Runtime/Enums.cs
+++ Assets/Code/Runtime/Enums.cs
@@ -1,6 +1,8 @@
 namespace Vheos.Games.ActionPoints
 {
     using System;
+    using System.Linq;
+    using System.Collections.Generic;
     using UnityEngine;
     using Games.Core;
 
@@ -19,7 +21,19 @@ namespace Vheos.Games.ActionPoints
 
     public enum ActionPhase
     {
+        Explore,
         Combat,
         Camp,
+    }
+    public enum ActionExecution
+    {
+        Instant,
+        Targeted,
+    }
+
+    public enum ActionAgent
+    {
+        User,
+        Target,
     }
 }
