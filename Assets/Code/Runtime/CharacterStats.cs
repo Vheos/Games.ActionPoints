93b88ab70f3b29ac0e1a3ca92f69fb69320b805f
Vheos <Vheos777@gmail.com>
2022-03-16 05:00:48 +0100

    added saving/loading pre-combat action progress

--- Assets/Code/Runtime/CharacterStats.cs
+++ Assets/Code/Runtime/CharacterStats.cs
@@ -13,31 +13,48 @@ namespace Vheos.Games.ActionPoints
         [field: SerializeField, Range(-1f, 1f)] public float ActionSpeed { get; private set; }
         [field: SerializeField, Range(-1f, 1f)] public float OverflowFocusRate { get; private set; }
         [field: SerializeField] public Action[] StartingActions { get; private set; }
+        [field: SerializeField] public PredefinedTeam StartingTeam { get; private set; }
 
         // Private
-        private Actionable _actionable;
+        private float _recentExplorationActionProgress;
+        private float _recentExplorationFocusProgress;
         private void ApplyActionSpeed()
-        {
-            _actionable.ActionProgress += Time.deltaTime * ActionSpeed;
-        }
+        => Get<Actionable>().ActionProgress += Time.deltaTime * ActionSpeed;
         private void ApplyOverflowFocusRate(float overflow)
+        => Get<Actionable>().FocusProgress += overflow * OverflowFocusRate;
+        private void HandleResourcesOnChangeCombat(Combat from, Combat to)
         {
-            _actionable.FocusProgress += overflow * OverflowFocusRate;
+            if (to != null)
+            {
+                _recentExplorationActionProgress = Get<Actionable>().ActionProgress;
+                _recentExplorationFocusProgress = Get<Actionable>().FocusProgress;
+                if (SettingsManager.Gameplay.ResetActionPointsWhenStartingCombat)
+                    Get<Actionable>().ActionProgress = Get<Actionable>().FocusProgress = 0;
+                Get<Updatable>().OnUpdate.Sub(ApplyActionSpeed);
+            }
+            else
+            {
+                Get<Actionable>().ActionProgress = _recentExplorationActionProgress;
+                Get<Actionable>().FocusProgress = _recentExplorationFocusProgress;
+                Get<Updatable>().OnUpdate.Unsub(ApplyActionSpeed);
+            }
         }
 
         // Play
         protected override void PlayStart()
         {
             base.PlayStart();
-            _actionable = Get<Actionable>();
-
-            _actionable.MaxActionPoints = MaxActionPoints;            
-            _actionable.LockedMaxActionPoints.Set(() => Get<Woundable>().Wounds);
-            _actionable.TryChangeActions(null, StartingActions);
-            Get<Updatable>().OnUpdate.SubEnableDisable(this, ApplyActionSpeed);
-            _actionable.OnOverflowActionProgress.SubEnableDisable(this, ApplyOverflowFocusRate);
+            Get<Actionable>().MaxActionPoints = MaxActionPoints;
+            Get<Actionable>().LockedMaxActionPoints.Set(() => Get<Woundable>().Wounds);
+            Get<Actionable>().TryChangeActions(null, StartingActions);
+            Get<Actionable>().OnOverflowActionProgress.SubEnableDisable(this, ApplyOverflowFocusRate);
 
             Get<Woundable>().MaxWounds.Set(() => MaxActionPoints);
+
+            Get<Combatable>().OnChangeCombat.SubEnableDisable(this, HandleResourcesOnChangeCombat);
+            Get<Teamable>().Team = Team.GetPredefinedTeam(StartingTeam);
+
+            Get<Actionable>().ActionPoints = MaxActionPoints - 1;
         }
     }
 }
